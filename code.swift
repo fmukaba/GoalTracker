ffff45a087490f2d45a8a2d7eb5f114ddf31d799
Francois (Franck) Mukaba <fxkikomina@gmail.com>
2019-08-08 10:37:06 -0700

    Update code.swift

--- code.swift
+++ code.swift
@@ -1,1 +1,3 @@
-// All code to build this app is property of Microsoft Core365 and cannot be disclosed.
+Warning : Do not try to reproduce this app
+          All code to build this app is property of Microsoft Core365 and cannot be disclosed.
+
